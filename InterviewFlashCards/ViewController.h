f5923763497404c71daedb6d10783f2ffbe4a112
Varindra <varindrahart@gmail.com>
2016-02-11 22:00:39 -0500

    first

---
+++ InterviewFlashCards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewFlashCards
+//
+//  Created by Varindra Hart on 2/11/16.
+//  Copyright © 2016 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InterviewFlashCards/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InterviewFlashCards
+//
+//  Created by Varindra Hart on 2/11/16.
+//  Copyright © 2016 Varindra Hart. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Firebase.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    Firebase *ref = [[Firebase alloc]initWithUrl:@"https://fiery-torch-4131.firebaseio.com/"];
+
+    [[ref queryOrderedByKey] observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
+        NSLog(@"%@ with value %@", snapshot.key, snapshot.value);
+    }];
+}
+
+
+@end
