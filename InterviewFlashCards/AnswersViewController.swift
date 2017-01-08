fdaf774457e583016ac96810c7c1bc00b60bb8ce
charles <charleshkang1@gmail.com>
2017-01-08 15:09:20 -0500

    removed all unnecessary uses of , deleted unused code for now, safely unwarapped forced optionals

--- InterviewFlashCards/AnswersViewController.swift
+++ InterviewFlashCards/AnswersViewController.swift
@@ -38,133 +38,47 @@ class AnswersViewController: UITableViewController {
     
     // MARK: Setup
     func setupUI() {
-        self.paginationLabel.text = ""
-        self.setupTempImageViewBounds()
-        if (self.flashCard.answerImages != nil) {
-            self.index = 0
-            self.answerImageView.isUserInteractionEnabled = true
-            self.answerImageView.image = self.flashCard.answerImages[0] as? UIImage
-        }
-        if (self.flashCard.answer != nil) {
-            self.answerLabel.text = self.flashCard.answer
-        }
-        else {
-            self.answerLabel.text = ""
+        paginationLabel.text = ""
+        setupTempImageViewBounds()
+        if (flashCard.answerImages != nil) {
+            index = 0
+            answerImageView.isUserInteractionEnabled = true
+            answerImageView.image = flashCard.answerImages[0] as? UIImage
+        } else if flashCard.answer == nil {
+            answerLabel.text = ""
+        } else {
+            answerLabel.text = flashCard.answer
         }
     }
     
     func setupTempImageViewBounds() {
-        var width: CGFloat = self.answerImageView.bounds.size.width
-        var height: CGFloat = self.answerImageView.bounds.size.height
-        var x: CGFloat = self.answerImageView.bounds.origin.x
-        var y: CGFloat = self.answerImageView.bounds.origin.y
-        self.tempAnswerImageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: height))
+        tempAnswerImageView = UIImageView(frame: answerImageView.frame)
     }
     
     func setupGestures() {
-        var leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.handleLeftSwipe))
-        leftSwipe.direction = .left
-        self.leftGesture = leftSwipe
-        var rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.handleRightSwipe))
-        rightSwipe.direction = .right
-        self.rightGesture = rightSwipe
-        var tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTapGesture))
-        self.tapGesture = tapGesture
-        var pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(self.handlePinching))
-        
-        view.addGestureRecognizer(leftSwipe)
-        view.addGestureRecognizer(rightSwipe)
-        view.addGestureRecognizer(tapGesture)
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinching))
         answerImageView.addGestureRecognizer(pinchGesture)
     }
     
     func showPaginationLabel() {
-        self.paginationLabel.text = "\(self.index + 1)/\(self.flashCard.answerImages.count)"
-    }
-    
-    // MARK: Swipe Handlers
-    
-    func handleLeftSwipe(_ gesture: UISwipeGestureRecognizer) {
-        if self.index < self.flashCard.answerImages.count - 1 {
-            self.index += 1
-            self.showPaginationLabel()
-            self.answerImageView.image = self.flashCard.answerImages[self.index] as? UIImage
-        }
-    }
-    
-    func handleRightSwipe(_ gesture: UISwipeGestureRecognizer) {
-        if self.index > 0 {
-            self.index -= 1
-            self.showPaginationLabel()
-            self.answerImageView.image = self.flashCard.answerImages[self.index] as? UIImage
-        }
+        paginationLabel.text = "\(index + 1)/\(flashCard.answerImages.count)"
     }
     
     func handlePinching(_ gesture: UIPinchGestureRecognizer) {
         if gesture.state == .changed || gesture.state == .ended {
-            gesture.view?.transform = (gesture.view?.transform.scaledBy(x: gesture.scale, y: gesture.scale))!
-            gesture.scale = 1
-        }
-    }
-    
-    func handleTapGesture(_ gesture: UITapGestureRecognizer) {
-        if !self.isTapped {
-            self.isTapped = true
-            self.backButton.isHidden = true
-            self.nextButton.isHidden = true
-            if !(self.flashCard.answer != nil) {
-                self.answerLabel.isHidden = true
-                self.answer.isHidden = true
-                self.answerImageView.frame = self.fullscreenFrame()
-            }
-            else if !(flashCard.answerImages != nil) {
-                self.answerImageView.isHidden = true
-                self.answer.isHidden = true
-                self.answerLabel.frame = self.fullscreenFrame()
+            if let gestureView = (gesture.view?.transform.scaledBy(x: gesture.scale, y: gesture.scale)) {
+                gesture.view?.transform = gestureView
+                gesture.scale = 1
             }
         }
-        else {
-            self.isTapped = false
-            self.backButton.isHidden = false
-            self.nextButton.isHidden = false
-            if !(self.flashCard.answer != nil) {
-                self.answerLabel.isHidden = false
-                self.answer.isHidden = false
-                self.answerImageView.bounds = (self.tempAnswerImageView?.bounds)!
-            }
-            else if !(self.flashCard.answerImages != nil) {
-                self.answerImageView.isHidden = false
-                self.answer.isHidden = false
-                self.answerLabel.frame = CGRect(x: CGFloat(10), y: CGFloat(60), width: CGFloat(self.view.bounds.size.width - 20), height: CGFloat(self.view.bounds.size.height - 20))
-            }
-        }
-    }
-    
-    //MARK: Score Tracker Alert
-    func displayScoreTrackerAlert() {
-        var controller = UIAlertController(title: "Score Check Time", message: "Did you get it?", preferredStyle: .alert)
-        var yesAction = UIAlertAction(title: "YES", style: .default, handler: {(_ action: UIAlertAction) -> Void in
-            self.dismiss(animated: false, completion: { _ in })
-        })
-        var noAction = UIAlertAction(title: "NO", style: .default, handler: {(_ action: UIAlertAction) -> Void in
-            self.dismiss(animated: false, completion: { _ in })
-        })
-        controller.addAction(yesAction)
-        controller.addAction(noAction)
-        self.present(controller, animated: true, completion: { _ in })
     }
     
     //MARK: Frame Maker
     func fullscreenFrame() -> CGRect {
-        return CGRect(x: CGFloat(self.view.bounds.origin.x + 10), y: CGFloat(self.view.bounds.origin.y + 10), width: CGFloat(self.view.bounds.size.width - 20), height: CGFloat(self.view.bounds.size.height - 10))
+        return CGRect(x: CGFloat(view.bounds.origin.x + 10), y: CGFloat(view.bounds.origin.y + 10), width: CGFloat(view.bounds.size.width - 20), height: CGFloat(view.bounds.size.height - 10))
     }
-    
     //MARK: Navigation
-    @IBAction func backButtonTapped(_ sender: UIButton) {
-        self.displayScoreTrackerAlert()
-    }
-    
-    @IBAction func nextButtonTapped(_ sender: UIButton) {
-        self.displayScoreTrackerAlert()
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 }
--- InterviewFlashCards/QuestionsViewController.swift
+++ InterviewFlashCards/QuestionsViewController.swift
@@ -12,39 +12,39 @@ enum SectionQuestionType {
     case iOSTechnical
     case dataStructures
     case algorithms
-
+    
 }
 
 class QuestionsViewController: UIViewController {
-
+    
     // MARK: IBOutlets
     @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var questionImageView: UIImageView!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerButton: UIButton!
-
+    
     // MARK: Private Properties
     fileprivate(set) var flashCards = [IFCFlashCard]()
     fileprivate var currentIndex = 0
     fileprivate var section: SectionQuestionType = .dataStructures
-
+    let segueIdentifier = "answerSegueIdentifier"
+    
     // MARK: Public Properties
     var sectionName = ""
     var dataGenerator: Networking? = QueryManager()
-
+    
     // MARK: Actions
-
     @IBAction fileprivate func prevButtonTapped(_ sender: UIButton) {
         currentIndex = currentIndex - 1 < 0 ? (currentIndex - 1 + flashCards.count) : 0
         prepareFlashCard(currentIndex)
     }
-
+    
     @IBAction fileprivate func nextButtonTapped(_ sender: UIButton) {
         currentIndex = (currentIndex + 1) % (flashCards.count)
         prepareFlashCard(currentIndex)
     }
-
+    
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,31 +52,31 @@ class QuestionsViewController: UIViewController {
         setupNavBar()
         fetchData()
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
-        if (segue.destination is AnswersViewController) {
+        if segue.identifier == segueIdentifier {
             (segue.destination as! AnswersViewController).flashCard = flashCards[currentIndex]
         }
     }
-
+    
     //MARK: Public Functions
     func set(sectionType value: SectionQuestionType) {
-            section = value
+        section = value
     }
-
+    
     func dismissController() {
-       _ = navigationController?.popViewController(animated: true)
+        _ = navigationController?.popViewController(animated: true)
     }
-
+    
     //MARK: Private functions
-
+    
     private func fetchData() {
         dataGenerator?.getData(for: requestType()) { [weak self] json in
             self?.flashCards = IFCFlashCard.flashCards(fromDictionaries: json)
             self?.prepareFlashCard(0)
         }
     }
-
+    
     private func setupNavBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Menu",
                                                            style: .plain,
@@ -84,7 +84,7 @@ class QuestionsViewController: UIViewController {
                                                            action: #selector(QuestionsViewController.dismissController))
         navigationItem.title = sectionName
     }
-
+    
     private func requestType() -> RequestType {
         switch (section) {
         case .iOSTechnical:
@@ -95,7 +95,7 @@ class QuestionsViewController: UIViewController {
             return .algorithms
         }
     }
-
+    
     fileprivate func prepareFlashCard(_ index: Int) {
         if let next = nextButton, let answer = answerButton {
             next.isHidden = true
@@ -106,7 +106,7 @@ class QuestionsViewController: UIViewController {
             self?.prepareUIwithCard(nextCard)
         }
     }
-
+    
     fileprivate func prepareUIwithCard(_ flashCard: IFCFlashCard) {
         if let question = flashCard.question {
             questionLabel.text = question
@@ -119,7 +119,7 @@ class QuestionsViewController: UIViewController {
         nextButton.isHidden = false
         answerButton.isHidden = false
     }
-
+    
     fileprivate func setAccessibilityLabels() {
         answerButton.accessibilityLabel = "answer button"
         nextButton.accessibilityLabel = "next button"
