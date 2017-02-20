3becba88b1e232d5e3dca5c8fcbb07a5024ddcb7
Varindra Hart <varindra@shopkeep.com>
2017-02-20 11:34:29 -0500

    Testing playground added

--- Playgrounds/AlgorithmTesting.playground/Contents.swift
+++ Playgrounds/AlgorithmTesting.playground/Contents.swift
@@ -1,5 +1,1 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
+"func isPalindrome(input: Int) -> Bool {#N#Tlet original = abs(input)#N#Tvar toBeReversed = original#N#Tvar reversed = 0#N#N#Twhile toBeReversed > 0 {#N#T#Tlet lastDigit = toBeReversed % 10#N#T#Treversed *= 10#N#T#Treversed += lastDigit#N#T#TtoBeReversed /= 10#N#T}#N#T#N#Treturn original == reversed#N}#N"
