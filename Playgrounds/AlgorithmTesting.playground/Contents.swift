caed9e4d2e0cd6f0e693ab9277b47b20f4077bb4
Varindra Hart <varindra@shopkeep.com>
2017-02-26 18:14:43 -0500

    Up and running

--- Playgrounds/AlgorithmTesting.playground/Contents.swift
+++ Playgrounds/AlgorithmTesting.playground/Contents.swift
@@ -1,1 +1,4 @@
-"func isPalindrome(input: Int) -> Bool {#N#Tlet original = abs(input)#N#Tvar toBeReversed = original#N#Tvar reversed = 0#N#N#Twhile toBeReversed > 0 {#N#T#Tlet lastDigit = toBeReversed % 10#N#T#Treversed *= 10#N#T#Treversed += lastDigit#N#T#TtoBeReversed /= 10#N#T}#N#T#N#Treturn original == reversed#N}#N"
+//
+// - Use this playground to write and test algorithm solutions
+//
+//
